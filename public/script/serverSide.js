ca3dae245ef147f2574de26946ee53a2cc329779
chinmayyyyyy <chinmayghayal@gmail.com>
2024-02-11 06:30:17 +0530

    done various changes

--- public/script/serverSide.js
+++ public/script/serverSide.js
@@ -1,8 +1,8 @@
 function patientSignup() {
     // Replace 'newpage.html' with the URL of the page you want to redirect to
-    window.location.href = '/signUp.html'; // Assuming 'signUp.html' is in the root directory
+    window.location.href = 'signUp.html';
 }
 function docSignup() {
     // Replace 'newpage.html' with the URL of the page you want to redirect to
-    window.location.href = '/docSignUp.html'; // Assuming 'docSignUp.html' is in the root directory
+    window.location.href = 'docSignUp.html';
 }
